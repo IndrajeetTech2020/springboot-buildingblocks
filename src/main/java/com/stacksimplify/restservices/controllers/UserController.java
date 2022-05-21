a74e4df91840e432fa6512840f3cd63cde1d030a
IndrajeetTech2020 <indrajeetpradhan1998@gmail.com>
2022-05-21 15:32:20 +0530

    First Commit - OneToMany

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -29,20 +30,21 @@ import com.stacksimplify.restservices.services.UserService;
 
 @RestController
 @Validated
+@RequestMapping(value="/users")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
 	//GetAllUsers method
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getAllUsers(){
 		return userService.getAllUsers();
 	}
 	
 	//Create user
 	//@RequestBody, @PostMapping
-	@PostMapping("/users")
+	@PostMapping
 	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, UriComponentsBuilder builder) {
 		try {
 			userService.createUser(user);
@@ -55,9 +57,8 @@ public class UserController {
 		}
 	}
 	
-	
 	//Get user by id
-	@GetMapping("/users/{id}")
+	@GetMapping("/{id}")
 	public Optional<User> getUserById(@PathVariable("id") @Min(1) Long id){
 		try {
 			return userService.getUserById(id);
@@ -67,7 +68,7 @@ public class UserController {
 	}
 	
 	//Update user by id
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
 		try {
 			return userService.updateUserById(id, user);
@@ -77,14 +78,14 @@ public class UserController {
 	}
 	
 	//Delete by user id
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUserById(@PathVariable("id") Long id) {
 		userService.deleteUserById(id);
 	}
 	
 	
 	//Get User By Username
-	@GetMapping("/users/byusername/{username}")
+	@GetMapping("/byusername/{username}")
 	public User getUserByUsername(@PathVariable("username") String username)throws UserNameNotFoundException {
 		User user = userService.getUserByUsername(username);
 		if(user == null) {
--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -1,9 +1,12 @@
 package com.stacksimplify.restservices.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -31,6 +34,9 @@ public class User {
 	private String role;
 	@Column(name="SSN", length = 50, nullable = false, unique = true )
 	private String ssn;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Order> order;
 	
 	//No Argument Constructor
 	public User() {
@@ -103,6 +109,14 @@ public class User {
 
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
+	}
+	
+	public List<Order> getOrder() {
+		return order;
+	}
+
+	public void setOrder(List<Order> order) {
+		this.order = order;
 	}
 
 	//To sting override
